7452590b242f2916406fd3ea73a0c0699bf4fbb2
Zoey Anastasiadis <zcanast@gmail.com>
2022-10-07 13:49:43 -0400

    lab5 updates

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -35,7 +35,9 @@ async function mainEvent() { // the async keyword means we can make API requests
       // It does not include any of your form values, though
     */
 
-    const results = await fetch('/api/foodServicePG');
+    //const results = await fetch('/api/foodServicePG');
+    const fetchQuery = new URLSearchParams(formProps);
+    const results = await fetch(`/api/foodServicePG?${fetchQuery}`);
     /*
    ## Get request with query parameters
 
