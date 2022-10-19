0fccc23d94494e59cfd6dba398a5ef80e4d51740
Zoey Anastasiadis <zcanast@gmail.com>
2022-10-19 16:08:45 -0400

    lab6 restaurant list

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -19,6 +19,17 @@ function getRandomInclusive(min, max) {
 
 function injectHTML(list) {
   console.log('fired injectHTML');
+  const target = document.querySelector("#restaurant_list");
+  target.innerHTML = '';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+
+  list.forEach((item) => {
+    const el = document.createElement('li');
+    el.innerText = item.name;
+    listEl.appendChild(el);
+  })
   /*
   ## JS and HTML Injection
     There are a bunch of methods to inject text or HTML into a document using JS
